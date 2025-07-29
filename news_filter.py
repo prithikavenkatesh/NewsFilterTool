0d368790c4ae9376c39a6636204c156a04e34d41
Prithika Venkatesh <prithikavenkatesh@consumer.org.hk>
2025-07-29 10:46:34 +0800

    Setting up NewsAPI

--- news_filter.py
+++ news_filter.py
@@ -1,3 +1,8 @@
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
+
+#API Setup
+URL = 'https://newsapi.org/v2/everything'
+
+from_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
