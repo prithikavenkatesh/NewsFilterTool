79ab2818bc0b955c7b0ff671989ce8dace6f9b10
Prithika Venkatesh <prithikavenkatesh@consumer.org.hk>
2025-07-29 11:14:23 +0800

    Collect Articles continued

--- news_filter.py
+++ news_filter.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
+from config_private import API_KEY
+from config_public import KEYWORDS, SELECTED_SOURCES
 
 # API Setup
 URL = 'https://newsapi.org/v2/everything'
@@ -17,3 +19,23 @@ for keyword in KEYWORDS:
         'language' : 'en',
         'apiKey' : API_KEY
     }
+
+    if SELECTED_SOURCES:
+        params['sources'] = SELECTED_SOURCES
+    
+    response = requests.get(URL, params=params)
+    data = response.json()
+
+    if data.get('status') != 'ok':
+        print(f"Error fetching articles for {keyword}': {data.get('message')} ")
+        continue
+    
+    print(f"Keyword: {keyword} - Found: {len(data.get('articles', []))} articles")
+    for article in data.get('articles', []):
+        articles.append({
+            'Date': article['publishedAt'][:10],
+            'Headline' : article['title'],
+            'Source' : article['source']['name'],
+            'Summary' : article['description'],
+            'Link' : article['url']
+        })
