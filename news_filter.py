b954b69738a5c16b3cec9bdddd359375d226f001
Prithika Venkatesh <prithikavenkatesh@consumer.org.hk>
2025-07-29 11:04:56 +0800

    Collect articles

--- news_filter.py
+++ news_filter.py
@@ -2,7 +2,18 @@ import requests
 import pandas as pd
 from datetime import datetime, timedelta
 
-#API Setup
+# API Setup
 URL = 'https://newsapi.org/v2/everything'
 
 from_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
+
+# Collect articles
+articles = []
+for keyword in KEYWORDS:
+    params = {
+        'q' : keyword,
+        'from' : from_date,
+        'sortBy' : 'relevancy',
+        'language' : 'en',
+        'apiKey' : API_KEY
+    }
