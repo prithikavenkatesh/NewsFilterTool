54411d844d3667e18210b8af248095e9fd1f02f2
Prithika Venkatesh <prithikavenkatesh@consumer.org.hk>
2025-07-29 15:56:20 +0800

    Updated for users to add their API key

---
+++ config_private.py
@@ -0,0 +1,6 @@
+
+# Type your personal API key below. 
+# You may choose to add this file to your .gitignore file to avoid publishing this personal information online.
+API_KEY = "your_api_key_here"
+
+
